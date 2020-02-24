0967f3d999036110acf2c537bc75e14f22ec8cc1
Double Black <davish@catlin.edu>
2020-02-24 13:01:41 -0800

    Add EventManager

--- src/main/java/io/github/anarchea/Anarchea.java
+++ src/main/java/io/github/anarchea/Anarchea.java
@@ -1,7 +1,21 @@
 package main.java.io.github.anarchea;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Anarchea extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(new EventManager(),this);
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Anarchea successfully enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Anarchea successfully disabled!");
+    }
 
 }
---
+++ src/main/java/io/github/anarchea/EventManager.java
@@ -0,0 +1,7 @@
+package main.java.io.github.anarchea;
+
+import org.bukkit.event.Listener;
+
+class EventManager implements Listener {
+
+}
